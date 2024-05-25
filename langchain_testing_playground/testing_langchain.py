6aa49f386a120e6642d0ef4b54a7f66a7cff6b55
Benedikt Willecke <benediktwillecke@gmail.com>
2024-05-25 17:11:22 +0200

    add usage of templates

--- langchain_testing_playground/testing_langchain.py
+++ langchain_testing_playground/testing_langchain.py
@@ -2,9 +2,19 @@ from langchain_groq import ChatGroq
 from dotenv import load_dotenv, find_dotenv
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain_core.prompts.prompt import PromptTemplate
 
 load_dotenv(find_dotenv())
-chat = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768")
-memory=ConversationBufferWindowMemory(k=5)
-conversation = ConversationChain(llm=chat, memory=memory)
+groq = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768")
+memory=ConversationBufferWindowMemory(k=5, ai_prefix="AI Assistant")
+template = """
+ The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+
+Current conversation:
+{history}
+Human: {input}
+AI Assistant:
+"""
+prompt = PromptTemplate(input_variables=["history", "input"], template=template)
+conversation = ConversationChain(prompt=prompt, llm=groq, memory=memory)
 print(conversation.invoke("tell me a joke")["response"])
