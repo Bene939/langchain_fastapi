d27d14f8ae109b7d05b66ce49a4d809ec6f5f6ee
Benedikt Willecke <benediktwillecke@gmail.com>
2024-05-26 18:33:20 +0200

    add basic streamlit app

--- langchain_testing_playground/testing_langchain.py
+++ langchain_testing_playground/testing_langchain.py
@@ -8,7 +8,7 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 import pickle
 from langchain.chains import ConversationalRetrievalChain
-import sys
+import streamlit as st
 
 #setup env
 load_dotenv(find_dotenv())
@@ -43,15 +43,28 @@ prompt = PromptTemplate(input_variables=["context", "question"], template=templa
 conversation = ConversationalRetrievalChain.from_llm(llm=llm, memory=memory, retriever=vectorstore.as_retriever(), combine_docs_chain_kwargs={"prompt": prompt})
 
 #setup chat
-print("AI Assistant: Hello! How can I assist you today? Type 'exit' to end the chat.")
+st.title('Scalable Capital FAQ Bot')
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-while True:
-    query = input("You: ")
-    if query.lower() == "exit":
-        print("AI Assistant: Goodbye!")
-        break
-    try:
-        response = conversation.invoke(query)
-        print("AI Assistant:", response["answer"])
-    except Exception as e:
-        print("An error occurred:", e)
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+st.session_state["conversation"] = conversation
+
+# React to user input
+if user_prompt := st.chat_input("Do you offer joint accounts?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(user_prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": user_prompt})
+
+    response = st.session_state["conversation"].invoke(user_prompt)["answer"]
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
