41d4be9ed895effa75f1b80ef054bcf05ac69eff
Benedikt Willecke <benediktwillecke@gmail.com>
2024-05-26 19:38:08 +0200

    add fastapi backend

---
+++ fastapi_backend.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from language_model import load_conversation_chain
+import uvicorn
+
+app = FastAPI()
+assistant = load_conversation_chain()
+
+@app.get("/assistant_response")
+def assistant_response(question: str):
+    return assistant.invoke(question)["answer"]
+
+uvicorn.run(app, host="127.0.0.1", port=8000)
--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -1,7 +1,5 @@
 import streamlit as st
-
-# Load vectorstore and conversation chain from language_model module
-from language_model import load_conversation_chain
+import requests
 
 # Setup chat
 st.title('Scalable Capital FAQ Bot')
@@ -15,9 +13,6 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Load conversation chain
-st.session_state["conversation"] = load_conversation_chain()
-
 # React to user input
 if user_prompt := st.chat_input("Do you offer joint accounts?"):
     # Display user message in chat message container
@@ -25,8 +20,9 @@ if user_prompt := st.chat_input("Do you offer joint accounts?"):
         st.markdown(user_prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": user_prompt})
+    url = f"http://127.0.0.1:8000/assistant_response?question={user_prompt}"
 
-    response = st.session_state["conversation"].invoke(user_prompt)["answer"]
+    response = requests.get(url).json()
     with st.chat_message("assistant"):
         st.markdown(response)
     # Add assistant response to chat history
