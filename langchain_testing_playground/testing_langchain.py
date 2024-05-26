0aad923e0dcda0459e572659aee4f3b5cd9718e5
Benedikt Willecke <benediktwillecke@gmail.com>
2024-05-26 17:33:52 +0200

    add vectorstore for faq, add retrieval for qa

--- langchain_testing_playground/testing_langchain.py
+++ langchain_testing_playground/testing_langchain.py
@@ -3,18 +3,43 @@ from dotenv import load_dotenv, find_dotenv
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain_core.prompts.prompt import PromptTemplate
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+import pickle
+from langchain.chains import RetrievalQA
 
+#setup env
 load_dotenv(find_dotenv())
-groq = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768")
+
+#setup vector database
+loader = DirectoryLoader("./data", glob="**/*.txt", loader_cls=TextLoader)
+docs = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+documents = text_splitter.split_documents(docs)
+
+embeddings = HuggingFaceEmbeddings()
+vectorstore = FAISS.from_documents(documents, embeddings)
+with open("faq_vectorstore.pkl", "wb")as f:
+    pickle.dump(vectorstore, f)
+
+#load db
+with open("faq_vectorstore.pkl", "rb") as f:
+    vectorstore = pickle.load(f)
+
+#setup model
+llm = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768")
 memory=ConversationBufferWindowMemory(ai_prefix="AI Assistant")
 template = """
- Your are an AI investment advisor. Answer any questions the user might have truthfully. Don't ask questions.
+ Your are an AI assistant for scalable capital. Your should assist customers with questions they have.
 
-Current conversation:
-{history}
-Human: {input}
-AI Assistant:
+{context}
+
+Question: {question}
+Answer here:
 """
-prompt = PromptTemplate(input_variables=["history", "input"], template=template)
-conversation = ConversationChain(prompt=prompt, llm=groq, memory=memory)
-print(conversation.invoke("i want to invest. can you recommend an etf for me?")["response"])
+prompt = PromptTemplate(input_variables=["context", "question"], template=template)
+conversation = RetrievalQA.from_chain_type(llm=llm, retriever=vectorstore.as_retriever(), chain_type="stuff", chain_type_kwargs={"prompt": prompt})
+query = "Are joint accounts offered?"
+print(conversation.invoke(query))
